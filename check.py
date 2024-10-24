4db37efc60054e6ebd612a92c0d2adf565714233
arthur <arthur-illa@hotmail.com>
2024-10-24 14:17:52 -0300

    added host check for the system tray

---
+++ check.py
@@ -0,0 +1,25 @@
+import sys
+import threading
+import time
+
+import pystray
+from icmplib import ping
+from PIL import Image
+
+
+def ping_host():
+    while True:
+        host = sys.argv[1]
+        response = ping(host, count=10, privileged=False)
+        if response.packet_loss >= 0.5:
+            icn = Image.new("RGB", (32, 32), (255, 0, 0))
+        else:
+            icn = Image.new("RGB", (32, 32), (0, 255, 0))
+        
+        icon.icon = icn
+        time.sleep(5)
+
+icon = pystray.Icon('ping', icon=Image.new("RGB", (32, 32), (255, 255, 255)))
+
+threading.Thread(target=ping_host).start()
+icon.run()
