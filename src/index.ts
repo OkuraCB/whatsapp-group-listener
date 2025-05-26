e32187a0d3d8ef6b887f835aa6c2e85bf5725202
Arthur Illa <arthur-illa@hotmail.com>
2025-05-25 23:06:02 -0300

    maintenance

--- src/index.ts
+++ src/index.ts
@@ -97,13 +97,16 @@ client.on("message_create", async (msg) => {
 
       case "!py":
         await chat.sendMessage(
-          "https://github.com/1sa4c/pyzzachat\nAcesse já utilizando o arquivo `main.py` dentro de `client` no IP arthurtv.duckdns.org e porta 10004!"
+          "https://github.com/1sa4c/pyzzachat\nAcesse já utilizando o arquivo `main.py` dentro de `client` no IP arthurtv.duckdns.org e porta 9000!"
         );
         break;
 
       case "!minecraft":
+        // await chat.sendMessage(
+        //   "Então você quer entrar no servidor do Xurso de Minecraft? Aqui vão as informações:\n- Minecraft 1.21.4;\n- IP: `arthurtv.duckdns.org`;\n- Porta (para Minecraft Java): `25565`;\n- Porta (para Minecraft Bedrock): `19132`"
+        // );
         await chat.sendMessage(
-          "Então você quer entrar no servidor do Xurso de Minecraft? Aqui vão as informações:\n- Minecraft 1.21.4;\n- IP: `arthurtv.duckdns.org`;\n- Porta (para Minecraft Java): `25565`;\n- Porta (para Minecraft Bedrock): `19132`"
+          "_Oh, that's unfortunate_\n\nEstamos em manutenção!"
         );
         break;
 
